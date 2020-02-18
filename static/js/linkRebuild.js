c809fd7ebb48261e0aca49ea84c5edb6b4b53ea0
BORYS PIDPERYGORA <bpidperygora@gmail.com>
2020-02-18 22:54:43 +0200

    Updates

--- static/js/linkRebuild.js
+++ static/js/linkRebuild.js
@@ -3,5 +3,6 @@
 var links = document.querySelectorAll('a');
 
 for (var a = 0; a < links.length; a++) {
-  links[a].href = 'mysite' + links[a].href;
+  var currentLink = links[a].getAttribute('href');
+  links[a].setAttribute('href', '/mysite' + currentLink);
 }
